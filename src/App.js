68eeb6d00cbcf3cab8b7be40d21d23a377ef7774
pongdang <pongdang0121@gmail.com>
2022-01-30 15:50:26 +0900

    220130

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Homepage from "./Component/Homepage";
+import Routerpage from "./Component/Routerpage";
 
 function App() {
   return (
     <div className='App'>
       <Homepage />
+      <Routerpage />
     </div>
   );
 }
---
+++ src/Component/Topics.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics ...
+    </div>
+  );
+}
