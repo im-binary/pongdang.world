32956e2724040372260d392213dff7a3429b4e88
pongdang <pongdang0121@gmail.com>
2022-02-04 16:49:29 +0900

    컴포넌트 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Homepage from "./Component/Homepage";
-import Routerpage from "./Component/Routerpage";
 
 function App() {
   return (
     <div className='App'>
       <Homepage />
-      <Routerpage />
     </div>
   );
 }
