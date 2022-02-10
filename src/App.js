64a5dfa4ec4139cb69219ab8263d5b05ba21061e
pongdang <pongdang0121@gmail.com>
2022-02-11 01:01:24 +0900

    feat: Title 컴포넌트 만들고 스타일 줬음

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Menu from "./components/Menu";
 import Router from "./components/Router";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Title />
         <Menu />
         <Router />
       </BrowserRouter>
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+export default function Title() {
+  return <H1>PONGDANG</H1>;
+}
+
+const H1 = styled.h1`
+  padding: 20px 0;
+  color: #0000cc;
+  letter-spacing: 30px;
+`;
