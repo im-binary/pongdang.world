bc34dbf9314fae8ff97f212c1f29a6a46f6de535
pongdang <pongdang0121@gmail.com>
2022-02-11 03:25:45 +0900

    chore: letter-spacing 대신 map() 이용하여 자간 조절함

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,14 +1,28 @@
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 import React from "react";
 import { mainTitleColor } from "../style/theme";
 
 export default function Title() {
-  return <H1>PONGDANG</H1>;
+  return (
+    <H1>
+      {"PONGDANG".split("").map((letter, i) => (
+        <span
+          key={`${letter}-${i}`}
+          css={css`
+            padding: 14px;
+          `}
+        >
+          {letter}
+        </span>
+      ))}
+    </H1>
+  );
 }
 
 const H1 = styled.h1`
   padding: 20px 0;
-  letter-spacing: 30px;
+  /* letter-spacing: 0.5em; */
   font-size: 50px;
   border: 1px solid olive;
   ${mainTitleColor};
