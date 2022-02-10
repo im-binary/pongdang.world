259237cc912c7683ce38e566dd05aa56d0b7b3fe
pongdang <pongdang0121@gmail.com>
2022-02-11 02:26:56 +0900

    feat: 메뉴 스타일 지정함

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
+import { mainMenuBackgroundClick, mainMenuBackground } from "../style/theme";
 
 export default function Menu() {
   return (
-    <nav>
-      <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/'>
-        Start
-      </NavLink>
-      <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/about'>
-        About
-      </NavLink>
-      <NavLink className={({ isActive }) => "nav-link" + (isActive ? " click" : "")} to='/contact'>
-        Contact
-      </NavLink>
-    </nav>
+    <WrapperDiv>
+      <nav>
+        <Ul>
+          <li>
+            <NavLink css={cssNavLink} to='/'>
+              Start
+            </NavLink>
+          </li>
+          <li>
+            <NavLink css={cssNavLink} to='/about'>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink css={cssNavLink} to='/contact'>
+              Contact
+            </NavLink>
+          </li>
+        </Ul>
+      </nav>
+    </WrapperDiv>
   );
 }
+
+const WrapperDiv = styled.div`
+  border: 1px solid olive;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+const cssNavLink = css`
+  padding: 20px 30px;
+  display: inline-block;
+  font-weight: bold;
+  ${mainMenuBackground}
+  &.active {
+    ${mainMenuBackgroundClick}
+  }
+`;
---
+++ src/style/theme.js
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+// TODO: 다크테마 구현해서 블로그에 기록 남기기
+
+export const mainTitleColor = css`
+  color: #0000cc;
+`;
+
+export const mainMenuBackground = css`
+  background-color: #1908a8;
+  color: white;
+`;
+
+export const mainMenuBackgroundClick = css`
+  background-color: #8dafff;
+`;
