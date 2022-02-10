c8222ff96bcbcb57297f608def0f88c0ae1c530c
pongdang <pongdang0121@gmail.com>
2022-02-11 02:27:23 +0900

    chore: Title 컴포넌트 스타일 지정

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import React from "react";
+import { mainTitleColor } from "../style/theme";
 
 export default function Title() {
   return <H1>PONGDANG</H1>;
@@ -7,6 +8,8 @@ export default function Title() {
 
 const H1 = styled.h1`
   padding: 20px 0;
-  color: #0000cc;
   letter-spacing: 30px;
+  font-size: 50px;
+  border: 1px solid olive;
+  ${mainTitleColor};
 `;
