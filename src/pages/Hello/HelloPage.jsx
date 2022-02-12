61e44a222c1e4257465bad80f3bbcf8ba6a6abd6
pongdang <pongdang0121@gmail.com>
2022-02-12 16:31:33 +0900

    feat: Hello 페이지 꾸몄음

--- src/pages/Hello/HelloPage.jsx
+++ src/pages/Hello/HelloPage.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { css } from "@emotion/react";
 
 export default function HelloPage() {
-  return <h1>HelloPage</h1>;
+  return (
+    <div css={contentContainer}>
+      <h1>👋 안녕하세요. 퐁당 입니다!</h1>
+    </div>
+  );
 }
+
+const contentContainer = css`
+  width: 100%;
+  border: 4px solid #f0f1f2;
+  margin: 10px 0;
+  padding: 20px;
+`;
