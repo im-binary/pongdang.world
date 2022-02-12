84ae19f5e6c2208924afd7d0eaa17e48c4659954
pongdang <pongdang0121@gmail.com>
2022-02-12 15:48:37 +0900

    chore: 필요없는 console.log 제거

--- src/hooks/title.js
+++ src/hooks/title.js
@@ -2,7 +2,6 @@ import { useLocation } from "react-router-dom";
 
 export function useTitle() {
   const { pathname } = useLocation();
-  console.log(useLocation());
   const title = pathname.split("/").pop();
   return title;
 }
