31cd88bd8e884cbeb601960c151d21ed249e50f6
pongdang <pongdang0121@gmail.com>
2022-02-14 23:47:38 +0900

    feat: mobile 화면에서의 메뉴

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,49 +1,79 @@
+import React from "react";
 import { css } from "@emotion/react";
-import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
 import { mainMenuBackgroundClick, mainMenuBackground, boldTitleFont } from "../style/theme";
 
 export default function Menu() {
   return (
-    <WrapperDiv>
-      <nav>
-        <ul css={menuContainer}>
-          <li>
-            <NavLink css={menuStyle} to='/studylog'>
-              공부기록
-            </NavLink>
-          </li>
-          <li>
-            <NavLink css={menuStyle} to='/'>
-              Hello
-            </NavLink>
-          </li>
-          <li>
-            <NavLink css={menuStyle} to='/contact'>
-              기록의 퐁당
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </WrapperDiv>
+    <>
+      {/* pc */}
+      <div css={pcWrapper}>
+        <nav>
+          <ul css={pcMenuContainer}>
+            <li>
+              <NavLink css={pcMenu} to='/studylog'>
+                공부기록
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={pcMenu} to='/'>
+                Hello
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={pcMenu} to='/contact'>
+                기록의 퐁당
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* mobile */}
+
+      <div css={mobileWrapper}>
+        <nav>
+          <ul css={mobileMenuContainer}>
+            <li>
+              <NavLink css={mobileMenu} to='/studylog'>
+                공부기록
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='/'>
+                Hello
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='/contact'>
+                기록의 퐁당
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
-const WrapperDiv = styled.div`
-  /* border: 1px solid olive; */
+const pcWrapper = css`
+  /* pc버전 menu가 500부터 안 보임 */
+  @media (max-width: 500px) {
+    display: none;
+  }
   ${boldTitleFont}
   font-size: 24px;
   margin-bottom: 30px;
 `;
 
-const menuContainer = css`
+const pcMenuContainer = css`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 30px;
 `;
 
-const menuStyle = css`
+const pcMenu = css`
   padding: 20px 30px;
   display: inline-block;
   line-height: 100%;
@@ -51,9 +81,32 @@ const menuStyle = css`
   &.active {
     ${mainMenuBackgroundClick}
   }
+`;
 
-  @media (max-width: 500px) {
-    font-size: 16px;
-    padding: 10px 20px;
+const mobileWrapper = css`
+  /* mobile 버전 menu가 500부터 안 보임 */
+  @media (min-width: 501px) {
+    display: none;
+  }
+  ${boldTitleFont}
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+const mobileMenuContainer = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const mobileMenu = css`
+  border: 1px solid gainsboro;
+  /* padding: 10px 15px; */
+  display: inline-block;
+  padding: 15px 20px;
+  line-height: 100%;
+  ${mainMenuBackground}
+  &.active {
+    ${mainMenuBackgroundClick}
   }
 `;
