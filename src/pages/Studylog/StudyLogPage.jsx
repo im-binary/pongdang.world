272802600657aac3769ae51aa4ff013a5673c1d3
pongdang <pongdang0121@gmail.com>
2022-02-15 23:57:10 +0900

    feat: mobile에서의 메뉴의 메뉴

--- src/pages/Studylog/StudyLogPage.jsx
+++ src/pages/Studylog/StudyLogPage.jsx
@@ -1,50 +1,140 @@
+import React from "react";
 import { css } from "@emotion/react";
 import { NavLink, Outlet } from "react-router-dom";
 import { mainMenuBackgroundClick, mainMenuBackground, boldTitleFont } from "../../style/theme";
 
 export default function StudyLogPage() {
   return (
-    <div css={wrapperDiv}>
-      <h1 style={{ display: "none" }}>공부기록</h1>
-      <nav css={navContainer}>
-        <ul css={menuContainer}>
-          <li>
-            <NavLink css={navLinkStyle} to='html'>
-              HTML
-            </NavLink>
-          </li>
-          <li>
-            <NavLink css={navLinkStyle} to='js'>
-              JS
-            </NavLink>
-          </li>
-          <li>
-            <NavLink css={navLinkStyle} to='react'>
-              React
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </div>
+    <>
+      {/* pc */}
+
+      <div css={pcWrapper}>
+        <h1 style={{ display: "none" }}>공부기록</h1>
+        <nav>
+          <ul css={pcMenuContainer}>
+            <li>
+              <NavLink css={pcMenu} to='html'>
+                HTML
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={pcMenu} to='js'>
+                JS
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={pcMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
+
+      {/* mobile */}
+
+      <div css={mobileWrapper}>
+        <h1 style={{ display: "none" }}>공부기록</h1>
+        <nav>
+          <ul css={mobileMenuContainer}>
+            <li>
+              <NavLink css={mobileMenu} to='html'>
+                HTML
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='js'>
+                JS
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                React
+              </NavLink>
+            </li>
+            <li>
+              <NavLink css={mobileMenu} to='react'>
+                XXXX
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
-const navContainer = css`
-  display: flex;
-  flex-direction: row;
-  @media (max-width: 699px) {
+// pc 버전
+
+const pcWrapper = css`
+  @media (max-width: 500px) {
     display: none;
   }
-`;
-
-const wrapperDiv = css`
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
 `;
 
-const menuContainer = css`
+const pcMenuContainer = css`
+  display: flex;
+  flex-direction: column;
   width: 200px;
   /* border: 1px solid tomato; */
   @media (max-width: 768px) {
@@ -52,7 +142,7 @@ const menuContainer = css`
   }
 `;
 
-const navLinkStyle = css`
+const pcMenu = css`
   display: block;
   padding: 10px 0;
   margin: 10px 0;
@@ -62,7 +152,27 @@ const navLinkStyle = css`
     ${mainMenuBackgroundClick}
   }
   ${boldTitleFont}
-  @media (max-width: 699px) {
+`;
+
+// mobile 버전
+
+const mobileWrapper = css`
+  @media (min-width: 501px) {
     display: none;
   }
+  background-color: gray;
+  border: 3px solid green;
+`;
+
+const mobileMenuContainer = css`
+  background-color: orange;
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  gap: 10px;
+  overflow: scroll;
+`;
+
+const mobileMenu = css`
+  background-color: lemonchiffon;
 `;
