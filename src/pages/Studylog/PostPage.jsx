dbdbee7b15bb0a3b8070d423b3997e1695be6167
pongdang <pongdang0121@gmail.com>
2022-02-16 00:33:30 +0900

    feat: mobile 버전 post 화면

--- src/pages/Studylog/PostPage.jsx
+++ src/pages/Studylog/PostPage.jsx
@@ -13,24 +13,44 @@ export default function PostPage() {
   }, [id]);
 
   return (
-    <div css={WrapperDiv}>
-      <h1 css={postTitle}>{id}</h1>
-      <div dangerouslySetInnerHTML={{ __html: contents }}></div>
-    </div>
+    <>
+      <div css={pcWrapper}>
+        <h1 css={pcPostTitle}>{id}</h1>
+        <div dangerouslySetInnerHTML={{ __html: contents }}></div>
+      </div>
+      <div css={mobileWrapper}>
+        <h1 css={mobilePostTitle}>{id}</h1>
+        <div dangerouslySetInnerHTML={{ __html: contents }}></div>
+      </div>
+    </>
   );
 }
 
-const WrapperDiv = css`
-  display: flex;
-  flex-direction: column;
+const pcWrapper = css`
+  @media (max-width: 699px) {
+    display: none;
+  }
   width: 100%;
-  border: 4px solid #f0f1f2;
+  border: 4px solid #d0d0d0;
   margin: 10px 0 0 10px;
   padding: 30px 30px;
   text-align: start;
   width: 100%;
 `;
 
-const postTitle = css`
+const pcPostTitle = css`
   margin: 0 0 30px;
+`;
+
+const mobileWrapper = css`
+  @media (min-width: 700px) {
+    display: none;
+  }
+  border: 3px solid #d0d0d0;
+  text-align: start;
+  padding: 16px 20px;
+`;
+
+const mobilePostTitle = css`
+  margin-bottom: 14px;
 `;
