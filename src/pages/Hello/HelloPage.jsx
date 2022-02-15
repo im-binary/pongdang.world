7a62753cb1ee38671829f2d99c755b1cc82cb936
pongdang <pongdang0121@gmail.com>
2022-02-16 00:47:30 +0900

    feat: 화면이 500px일 때부터 폰트 크기 조정 

--- src/pages/Hello/HelloPage.jsx
+++ src/pages/Hello/HelloPage.jsx
@@ -4,14 +4,24 @@ import { css } from "@emotion/react";
 export default function HelloPage() {
   return (
     <div css={contentContainer}>
-      <h1>👋 안녕하세요. 퐁당 입니다!</h1>
+      <h1>안녕하세요👋 퐁당의 블로그 입니다.</h1>
+      <p></p>
     </div>
   );
 }
 
 const contentContainer = css`
   width: 100%;
-  border: 4px solid #f0f1f2;
+  border: 4px solid #d0d0d0;
   margin: 10px 0;
   padding: 20px;
+  @media (max-width: 501px) {
+    border: 3px solid #d0d0d0;
+  }
+
+  h1 {
+    @media (max-width: 501px) {
+      font-size: 22px;
+    }
+  }
 `;
