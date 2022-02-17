61a56b951d43caeaf4a5e3e579ca8d1050c9dc9d
pongdang <pongdang0121@gmail.com>
2022-02-17 10:41:50 +0900

    faet: 검색창 만들기,, 아직 못함

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { css } from "@emotion/react";
 import { boldTitleFont } from "../style/theme";
+// import Information from "../constants/Infomation";
 
 export default function Search() {
+  const handleSearchChange = (e) => {
+    console.log(e.target.value.toLowerCase());
+  };
+
+  // const items = Information.map((data) => {
+  //   return (
+  //     <div>
+  //       <ul>
+  //         <li>{data.title}</li>
+  //         <li>{data.category}</li>
+  //       </ul>
+  //     </div>
+  //   );
+  // });
+
   return (
     <div>
-      <input type='text' css={inputStyle} placeholder='검색어를 입력해주세요' />
+      <input onChange={handleSearchChange} type='text' css={inputStyle} placeholder='검색어를 입력해주세요' />
+      {/* <div>{items}</div> */}
     </div>
   );
 }
---
+++ src/constants/Infomation.js
@@ -0,0 +1,12 @@
+const Infomation = [
+  {
+    title: "첫 번째 게시물",
+    category: "html",
+  },
+  {
+    title: "두 번째 게시물",
+    category: "html",
+  },
+];
+
+export default Infomation;
