57f6b612be70af0ff896b2e98824c80f1e16d080
pongdang <pongdang0121@gmail.com>
2022-02-27 18:34:00 +0900

    chore: comment

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# md 파일을 HTML 파일로 변환
 find ./static-post -name "*.md" | xargs -I {FILE} marked -i {FILE} -o ./public/{FILE}.html
 
+# HTML 파일 앞에 붙일 prefix 를 위한 스크립트 조각
 SCRIPT=$(cat <<-END
 const fs = require('fs').promises;
 const prefix = \`<head>
@@ -10,4 +12,8 @@ const prefix = \`<head>
 END
 )
 
-find ./public/static-post -name "*.html" | grep -v "index.html" | xargs -I {FILE} node -p "$SCRIPT;fs.readFile('{FILE}').then((e) => fs.writeFile('{FILE}', prefix + '\n' + e))"
+# HTML 파일에 prefix 붙이고 파일 덮어쓰기
+find ./public/static-post -name "*.html" | \
+    grep -v "index.html" | \
+    xargs -I {FILE} \
+    node -p "$SCRIPT;fs.readFile('{FILE}').then((e) => fs.writeFile('{FILE}', prefix + '\n' + e))"
