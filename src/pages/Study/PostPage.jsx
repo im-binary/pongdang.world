ee3adc006fe6a96c60f572ceccba9229568afbf5
pongdang <pongdang0121@gmail.com>
2022-02-28 22:33:04 +0900

    feat: studylog 스타일

--- src/pages/Study/PostPage.jsx
+++ src/pages/Study/PostPage.jsx
@@ -4,6 +4,31 @@ import { useCategory } from "../../hooks/category";
 import { css } from "@emotion/react";
 import { getPost } from "../../remotes/post";
 
+const mdStyle = css`
+  h1,
+  h2,
+  h3 {
+    margin: 20px 0;
+  }
+
+  p {
+    line-height: 1.8;
+    margin: 10px 0;
+  }
+
+  pre {
+    padding: 20px 30px;
+    background-color: #e9e9e9;
+    border-radius: 20px;
+    margin: 10px 0;
+    overflow: scroll;
+  }
+
+  img {
+    width: 100%;
+  }
+`;
+
 export default function PostPage() {
   const [contents, setContents] = useState("");
   const { id } = useParams();
@@ -16,12 +41,12 @@ export default function PostPage() {
   return (
     <>
       <div css={pcWrapper}>
-        <h1 css={pcPostTitle}>{id}</h1>
-        <div dangerouslySetInnerHTML={{ __html: contents }}></div>
+        {/* <h1 css={pcPostTitle}>{id}</h1> */}
+        <div css={mdStyle} dangerouslySetInnerHTML={{ __html: contents }}></div>
       </div>
       <div css={mobileWrapper}>
-        <h1 css={mobilePostTitle}>{id}</h1>
-        <div dangerouslySetInnerHTML={{ __html: contents }}></div>
+        {/* <h1 css={mobilePostTitle}>{id}</h1> */}
+        <div css={mdStyle} dangerouslySetInnerHTML={{ __html: contents }}></div>
       </div>
     </>
   );
@@ -32,26 +57,18 @@ const pcWrapper = css`
     display: none;
   }
   width: 100%;
-  border: 4px solid #d0d0d0;
+  border: 4px solid #8dafff;
   margin: 10px 0 0 10px;
-  padding: 30px 30px;
+  padding: 10px 30px;
   text-align: start;
   width: 100%;
-`;
-
-const pcPostTitle = css`
-  margin: 0 0 30px;
 `;
 
 const mobileWrapper = css`
   @media (min-width: 700px) {
     display: none;
   }
-  border: 3px solid #d0d0d0;
+  border: 3px solid #8dafff;
   text-align: start;
   padding: 16px 20px;
-`;
-
-const mobilePostTitle = css`
-  margin-bottom: 14px;
 `;
