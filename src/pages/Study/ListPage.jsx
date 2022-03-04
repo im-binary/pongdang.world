1ea07fb41d6e60a047f95067dca1c16329b34394
pongdang <pongdang0121@gmail.com>
2022-03-04 21:27:47 +0900

    fix: listpage 제목 이쁘게

--- src/pages/Study/ListPage.jsx
+++ src/pages/Study/ListPage.jsx
@@ -31,14 +31,17 @@ const WrapperDiv = styled.div`
   ${contentContainer};
 `;
 
-const listPageTitle = css`
-  color: #8dafff;
-`;
-
 const navLinkStyle = css`
   background-color: white;
-  border: 3px solid #d0d0d0;
+  /* border: 3px solid #d0d0d0; */
+  border-radius: 16px;
+  font-size: 20px;
+  font-weight: bold;
   margin: 20px 0;
   display: block;
   padding: 20px 0;
+  box-shadow: rgb(0 0 0 / 20%) 0px 4px 8px 0px, rgb(0 0 0 / 19%) 0px 6px 20px 0px;
+  &:hover {
+    background-color: #e0e0e0;
+  }
 `;
