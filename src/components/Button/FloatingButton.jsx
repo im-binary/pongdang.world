4e8ef1f1090dc33fd6c0f206fa123971c892ec28
pongdang <pongdang0121@gmail.com>
2022-04-02 22:17:08 +0900

    feat: 스크롤 위치에 따라 버튼 나타나기

--- src/components/Button/FloatingButton.jsx
+++ src/components/Button/FloatingButton.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 
 export default function FloatingButton() {
-  const handleUpClick = () => {
+  const [button, setButton] = useState(false);
+
+  useEffect(() => {
+    const updateButtonVisible = () => {
+      if (window.scrollY > 130) {
+        setButton(true);
+      } else {
+        setButton(false);
+      }
+    };
+    window.addEventListener("scroll", updateButtonVisible);
+
+    return () => {
+      window.removeEventListener("scroll", updateButtonVisible);
+    };
+  }, []);
+
+  const handleTopClick = () => {
     console.log("클릭");
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <div css={buttonStyle} onClick={handleUpClick}>
+    <button css={button ? visible : unvisible} onClick={handleTopClick}>
       <i className='fa-solid fa-circle-chevron-up'></i>
-    </div>
+    </button>
   );
 }
 
-const buttonStyle = css`
+const unvisible = css`
+  display: none;
+`;
+
+const visible = css`
+  opacity: 0.8;
+  border: none;
+  background-color: unset;
+  color: inherit;
+  display: block;
   position: fixed;
   bottom: 106px;
   right: 28px;
