24df08d80aea6296b160d1fc4c9a45ffbcb3090d
pongdang <pongdang0121@gmail.com>
2022-04-24 19:25:05 +0900

    feat: 문구 업데이트

--- src/pages/Hello/HelloPage.jsx
+++ src/pages/Hello/HelloPage.jsx
@@ -7,16 +7,18 @@ export default function HelloPage() {
       <h1>
         안녕하세요 👋 <br /> 퐁당의 블로그 입니당 👻
       </h1>
+
       <section>
-        <h3>🤔 이런 고민을 하는 개발자가 되고 싶어요</h3>
+        <h3>🤔 저는 이런 고민을 합니당</h3>
         <ul>
           <li>좋은 사용자 경험은 무엇일까?</li>
           <li>읽기 좋은 코드란 무엇일까?</li>
           <li>변경이 쉽고 확장에 용이한 코드는 무엇일까?</li>
         </ul>
       </section>
+
       <section>
-        <h3>🧑‍💻 아래 기술들을 열심히 배우고 있어요</h3>
+        <h3>🧑‍💻 아래 기술들을 찍먹해보았어요</h3>
         <ul>
           <li>HTML, CSS, JavaScript</li>
           <li>React, React Redux, Emotion</li>
@@ -26,7 +28,7 @@ export default function HelloPage() {
       </section>
 
       <section>
-        <h3>😍 전 이런 걸 좋아해요</h3>
+        <h3>😍 저는 이런 걸 좋아해요</h3>
         <ul>
           <li>😻 고양이를 좋아합니다.</li>
           <li>📚 책 읽는 걸 좋아합니다.</li>
